0f69bd9d64a8ec30dff1f561f79a16501e24795d
NadJoe <57606302+NadJoe@users.noreply.github.com>
2020-09-04 14:56:27 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-
-
-
 // Note: This example requires that you consent to location sharing when
 // prompted by your browser. If you see the error "The Geolocation service
 // failed.", it means you probably did not give permission for the browser to
@@ -18,13 +15,14 @@ function initMap() {
 
 
    // Try HTML5 geolocation.
+   
    if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(function (position) {
          let pos = {
             lat: position.coords.latitude,
             lng: position.coords.longitude
          };
-         console.log(position);
+
          // Information que contiendra l'infobulle sur le marker
          let infos = new google.maps.InfoWindow({
             content: '<h2>Position Actuelle</h2>',
@@ -43,115 +41,71 @@ function initMap() {
 
 
          map.setCenter(pos);
+
+         /*let service = new google.maps.places.PlacesService(map);
+         service.nearbySearch({
+            location : pos,
+            radius: 5500,
+            type : ['restaurant']
+         }, callback);
+
+         function callback(results, status){
+            if (status === google.maps.places.PlacesServiceStatus.OK) {
+                for (var i = 0; i < results.length; i++) {
+                    createMarker(results[i]);
+                }
+            }
+        }
+
+
+        function createMarker(place) {
+         var placeLoc = place.geometry.location;
+         var marker = new google.maps.Marker({
+             map : map,
+             position : place.geometry.location
+         });
+
+         google.maps.event.addListener(marker, 'click', function() {
+             infowindow.setContent(place.name);
+             infowindow.open(map, this);
+         });
+     }*/
+
+
       }, function () {
          //handleLocationError(true, infoWindow, map.getCenter());
          alert("ERROR");
       },{timeout:10000});
+      
    } else {
       // Browser doesn't support Geolocation
       handleLocationError(false, infoWindow, map.getCenter());
    }
 
+/*********************************************************************************************************/
+/*********************************************************************************************************/
+
    // Ajout des markers pour chaque restaurant avec la boucle forEach
-   lesRestos.forEach(function (resto) {
+   lesRestos.forEach(function (resto, index) {
       // Donner a la fonction addMarker les données des restos en paramètres
-      addMarker({
-         lat: resto.lat,
-         lng: resto.long
-      });
-      // fonction qui gère l'ajout des markers    
-      function addMarker(coords) {
-         let markerRest = new google.maps.Marker({
-            position: coords,
-            map: map
-         });
+      let restaurant = new Restaurant(index, resto);
 
-         // Information que contiendra l'infobulle sur le marker
-         let infosMarker = new google.maps.InfoWindow({
-            content: resto.restaurantName,
-         });
+      restaurant.setMarker(map);
+      restaurant.displayContent();
 
-         // Quand l'utilisateur clique sur le marker, l'info apparait.
-         markerRest.addListener('click', function () {
-            infosMarker.open(map, markerRest);
-         })
-      }
-
-
-      // TEST D'AFFICHAGE DES INFORMATIONS
-      let nom = document.createElement('div'); // creation d'une balise <li> qui va prendre les noms des restaurants
-      nom.classList.add('list-group-item'); // Donner une classe à la balise pour que les styles css bootstrap s'appliquent
-      nom.textContent = resto.restaurantName; // contenu textuel de notre balise
-      document.querySelector('.moment').appendChild(nom);
-
-      let getNoteMoyenne = ( rate )=>{
-         let total = 0;
-         rate.forEach(elt=>{
-            total+= elt.stars;
-         });
-         return total/rate.length;
-      } ;
-      let noteMoyenne = getNoteMoyenne(resto.ratings);
-
-
-      // GESTION D'AFFICHAGE DES ADRESSES
-      let addresse = document.createElement('div'); // div qui stocke les addresses des restaurants
-      addresse.classList.add('content'); // affectation d'une classe à la balise des addresses
-      addresse.textContent = "Adresse : " + resto.address; // contenu textuel de la balise des addresses
-      
-
-      // GESTION D'AFFICHAGE DES COORDONÉES
-      let latitude = document.createElement('div'); // div qui stocke les addresses des restaurants
-      latitude.textContent = "Lat : " + resto.lat; // contenu textuel de la balise des addresses
-      addresse.appendChild(latitude);
-      document.querySelector('.moment').appendChild(addresse);
-
-
-      let longitude = document.createElement('div'); // div qui stocke les addresses des restaurants
-      longitude.textContent = "Long : " + resto.long; 
-      addresse.appendChild(longitude);
-      document.querySelector('.moment').appendChild(addresse);
-
-      // GESTION DES COMMENTAIRES POUR L'AFFICHAGE
-      let commentaires = document.createElement('div'); // creation d'une balise <li> qui va prendre les noms des restaurants
-      commentaires.classList.add('comStyle'); // Donner une classe à la balise pour que les styles css bootstrap s'appliquent
-      commentaires.textContent = " Les commentaires "; // contenu textuel de notre balise
-      addresse.appendChild(commentaires);
-      document.querySelector('.moment').appendChild(addresse);
-
-      let note = document.createElement('div'); // div qui stocke les addresses des restaurants
-      //note.textContent = resto.ratings; 
-      note.innerHTML = resto.ratings.map(elt=>{
-         return ` <div> <p> Note : ${elt.stars} </p>  <p> Comment : ${elt.comment} </p> </div> `
-      });
-      note.innerHTML+=`<p> Note moyenne : ${noteMoyenne} </p>`;
-      addresse.appendChild(note);
-      document.querySelector('.moment').appendChild(addresse);
-
-
-      // GESTION DES COMMENTAIRES POUR L'ECRITURE
-      let readComment = document.createElement('div'); // creation d'une balise <li> qui va prendre les noms des restaurants
-      readComment.classList.add('writeComment'); // Donner une classe à la balise pour que les styles css bootstrap s'appliquent
-      readComment.textContent = " Ajouter"; // contenu textuel de notre balise
-      addresse.appendChild(readComment);
-      document.querySelector('.moment').appendChild(addresse);
-
-      
-      let writeInput = document.createElement('input');
-      addresse.appendChild(writeInput);
-      document.querySelector('.moment').appendChild(addresse);
-
-      /*let bac = document.getElementById("formulaire");
-      console.log(bac.innerHTML);*/
-
-      
+      let yes = document.getElementsByClassName("writeComment");
+      restaurant.writeComment(yes);
 
    });
-   
-   /*document.getElementById("formulaire").addEventListener("submit", function(e) {
-         e.preventDefault();
-         alert("Votre avis est envoyé !");
-      })*/
+
+
+/*********************************************************************************************************/
+/*********************************************************************************************************/
+
+
+/*********************************************************************************************************/
+/*********************************************************************************************************/
+
 // FONCTION COLLAPSE AU CLIC SUR UN ÉLÉMENT 
 function Collapse(params) {
    let coll = document.getElementsByClassName(params);
--- restaurant.js
+++ restaurant.js
@@ -1,20 +1,113 @@
-class restaurant {
+class Restaurant {
 
-    constructor(id, objetRestoJson) {
-       if (objetRestoJson) {
-          this.restaurantNom = objetRestoJson.restaurantName;
-          this.addresseResto = objetRestoJson.address;
-          this.avis = objetRestoJson.ratings;
-       } else {
-          // Initialiser le tratings a vide.
-          this.avis = [];
-       }
-       // Identifiant unique permettant de manipuler le DOM sidebar à partir de l'objet
-       this.id = "restaurant_" + id;
-       // Indice du li dans la liste ul : déroulant du li via Materialize
-       this.indice = id;
-       // Initialisation de la moyenne 
-       this.MoyenneNote = this.MoyNote;
- 
+    constructor(id, resto) {
+        this.id = `restaurant_${id}`;
+        this.restaurantName = resto.restaurantName;
+        this.address = resto.address;
+        this.lat = resto.lat;
+        this.long = resto.long;
+        this.ratings = resto.ratings;
+        this.averageNote = this.MoyNote;
+        this.marker = null;
+
     }
- }
+
+    // Methods Calcul note moyenne
+    getNoteMoyenne(rate){
+        let total = 0;
+        rate.forEach(elt=>{
+           total+= elt.stars;
+        });
+        return total/rate.length;
+     };
+
+     // Initialisation du model de marker
+     setMarker(map){
+        let marker = new google.maps.Marker({
+            position: {lat: this.lat, lng: this.long},
+            map: map
+        });
+
+            // Information que contiendra l'infobulle sur le marker
+        let infosMarker = new google.maps.InfoWindow({
+            content: this.restaurantName
+        });
+
+            // Quand l'utilisateur clique sur le marker, l'info apparait.
+        marker.addListener('click', function () {
+            infosMarker.open(map, marker);
+        })
+
+        this.marker = marker;
+     }
+
+
+     displayContent(){
+
+        let content = `
+                <div class="list-group-item">${this.restaurantName} </div>
+                <div class="content">
+                    <div>${this.address}</div>
+                    <div>${this.lat} </div>
+                    <div> ${this.long}</div>
+                    <div class="comStyle"> Les commentaires </div>
+                    <div class="nouveau">
+                        ${
+                            this.ratings.map(elt=>{
+                            return ` <div> <p> Note : ${elt.stars} </p>  <p> Comment : ${elt.comment} </p> </div> `
+                        }) 
+                        }
+                        <p> Note moyenne : ${this.getNoteMoyenne(this.ratings)} </p>
+                    </div>
+                    <div class = "writeComment" > Ajouter un commentaire</div>
+                </div>
+            
+            `
+        document.querySelector('.moment').innerHTML += content;
+    }
+
+    //let commentPlace = document.getElementsByClassName("writeComment");
+    writeComment(commentPlace){
+
+        for (let item of commentPlace) {
+            item.addEventListener("click", function(params) {
+               if(document.getElementById("formComment")===null){
+        
+                  let writeInput = document.createElement('div');
+                  writeInput.innerHTML = `<br><form id="formComment"> 
+                                          <input type="number" placeholder="Notez le restaurant" step="1" min="0" max="5" id="votreNote">
+                                          <input placeholder=" Votre commentaire " id="votreCom"> 
+                                          <input type="submit" value="Envoyer" >
+                                       </form>`;
+                                       params.target.parentNode.appendChild(writeInput);
+                                       document.getElementById("formComment").addEventListener("submit", function (e) {
+        
+        
+        
+                                          e.preventDefault();
+                                          let laNote = document.getElementById("votreNote").value;
+                                          let leCom = document.getElementById("votreCom").value;
+                                          
+                                          writeInput.parentNode.removeChild(writeInput);
+        
+                                          //console.log(document.getElementsByClassName('active')[0].textContent); // Restaurant sur lequel on veut ajouter un commentaire
+        
+                                          let nvCom = `,<br> <div> <p> Note : ${laNote} </p>  <p> Comment : ${leCom} </p> </div> <br>`
+ 
+                                          alert(this.ratings.comment);
+                                          
+                                          //document.querySelector(".nouveau").innerHTML += nvCom;
+        
+                                       });
+        
+               }
+               
+            });
+               
+         }
+    }
+}
+
+
+
+
